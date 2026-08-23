9f8c582e93452e73478c2743b202d77ff1d46303
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-23 10:41:44 +0900

    Enable V16 streamed scenery pass

--- src/sky/SkyDancerAirCombatFx.ts
+++ src/sky/SkyDancerAirCombatFx.ts
@@ -1,5 +1,5 @@
-// V15 is active and extends SkyDancerAirCombatFxV14 -> V13 -> V12 -> SkyDancerAirCombatFxV11.
+// V16 is active and extends SkyDancerAirCombatFxV15 -> V14 -> V13 -> V12 -> SkyDancerAirCombatFxV11.
 export {
-  SkyDancerAirCombatFxV15 as SkyDancerAirCombatFx,
-} from "./SkyDancerAirCombatFxV15";
+  SkyDancerAirCombatFxV16 as SkyDancerAirCombatFx,
+} from "./SkyDancerAirCombatFxV16";
 export type { SkyDancerFxRuntime } from "./SkyDancerAirCombatFxV2";
