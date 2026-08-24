adab20923677c40bfc54c9da732a79f2b879c712
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-24 11:35:47 +0900

    Connect V30 reference polish controller

--- src/sky/SkyDancerAirCombatFxV30.ts
+++ src/sky/SkyDancerAirCombatFxV30.ts
@@ -5,6 +5,7 @@ import type { SkyDancerFxRuntime } from "./SkyDancerAirCombatFxV2";
 import { SkyDancerGroundDetailV30 } from "./SkyDancerGroundDetailV30";
 import { SkyDancerLegacySceneryCleanupV30 } from "./SkyDancerLegacySceneryCleanupV30";
 import { installSkyDancerOpeningSpacingV30 } from "./SkyDancerOpeningSpacingV30";
+import { SkyDancerReferencePolishV30 } from "./SkyDancerReferencePolishV30";
 import { SkyDancerWorldPresentationV30 } from "./SkyDancerWorldPresentationV30";
 
 /**
@@ -16,6 +17,7 @@ export class SkyDancerAirCombatFxV30 extends SkyDancerAirCombatFxV29 {
   private readonly legacyCleanup: SkyDancerLegacySceneryCleanupV30;
   private readonly groundDetail: SkyDancerGroundDetailV30;
   private readonly worldPresentation: SkyDancerWorldPresentationV30;
+  private readonly referencePolish: SkyDancerReferencePolishV30;
 
   constructor(runtime: SkyDancerFxRuntime) {
     super(runtime);
@@ -25,6 +27,7 @@ export class SkyDancerAirCombatFxV30 extends SkyDancerAirCombatFxV29 {
     this.legacyCleanup = new SkyDancerLegacySceneryCleanupV30(runtime);
     this.groundDetail = new SkyDancerGroundDetailV30(runtime);
     this.worldPresentation = new SkyDancerWorldPresentationV30(runtime);
+    this.referencePolish = new SkyDancerReferencePolishV30(runtime);
   }
 
   override update(snapshot: CartArenaSessionSnapshot, missiles: SkyDancerMissileState, delta: number): void {
@@ -34,6 +37,7 @@ export class SkyDancerAirCombatFxV30 extends SkyDancerAirCombatFxV29 {
     this.legacyCleanup.update();
     this.groundDetail.update(snapshot);
     this.worldPresentation.update(snapshot);
+    this.referencePolish.update();
   }
 }
 
