bb2000b4fb7a902e76ef9c180bebd46b287a8aa0
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-24 10:07:51 +0900

    Connect V30 HUD consolidation to game page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import CartRogueGame from "./CartRogueGamePhase13";
 import ServiceWorkerRegistration from "./ServiceWorkerRegistration";
+import SkyDancerHudV30 from "./SkyDancerHudV30";
 
 export default function Page() {
   return <>
     <ServiceWorkerRegistration />
     <CartRogueGame />
+    <SkyDancerHudV30 />
   </>;
 }
