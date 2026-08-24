da2ff054997b199473d28a626650a195291b257b
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-24 11:47:58 +0900

    Mount V30 color grade in app runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import CartRogueGame from "./CartRogueGamePhase13";
 import ServiceWorkerRegistration from "./ServiceWorkerRegistration";
+import SkyDancerColorGradeV30 from "./SkyDancerColorGradeV30";
 import SkyDancerHudV30 from "./SkyDancerHudV30";
 
 export default function Page() {
   return <>
     <ServiceWorkerRegistration />
     <CartRogueGame />
+    <SkyDancerColorGradeV30 />
     <SkyDancerHudV30 />
   </>;
 }
