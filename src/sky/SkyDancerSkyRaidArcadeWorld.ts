e96c8bb62adde6271ff203c5c43f3d530639b20c
yz4git <65384484+yz4git@users.noreply.github.com>
2026-09-03 11:20:05 +0900

    Suppress remaining legacy scenery in SKY RAID

--- src/sky/SkyDancerSkyRaidArcadeWorld.ts
+++ src/sky/SkyDancerSkyRaidArcadeWorld.ts
@@ -4,11 +4,16 @@ import { skyDancerArcadeStageById, type SkyDancerArcadeStageDefinition, type Sky
 import type { SkyDancerSkyRaidAct } from "./SkyDancerSkyRaidRules";
 
 const LEGACY_ENV_PREFIXES = [
+  "sky-dancer-v23-",
+  "sky-dancer-v28-",
+  "sky-dancer-v30-",
+  "sky-dancer-v32-",
   "sky-dancer-v35-",
   "sky-dancer-v36-",
   "sky-dancer-v38-",
   "sky-dancer-v40-",
   "sky-dancer-v41-",
+  "sky-dancer-v42-",
   "sky-dancer-v47-",
   "sky-dancer-v50-",
   "sky-dancer-v53-",
@@ -38,6 +43,7 @@ function suppressLegacyEnvironment(scene: THREE.Scene): void {
   scene.traverse((object) => {
     if (insideArcadeEnvironment(object)) return;
     const legacyNamed = object.name === "phase67-turbo-hunt-world"
+      || object.name === "sky-dancer-legacy-environment"
       || LEGACY_ENV_PREFIXES.some((prefix) => object.name.startsWith(prefix));
     const legacyTheme = object.userData.skyDancerLegacyEnvironment === true;
     const legacyLargeSky = object instanceof THREE.Mesh
